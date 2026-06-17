dbff1b7cdbd3419cbb54b3b1057bb45559e4d4cc
Ananaya2309 <ananayabajaj4@gmail.com>
2026-06-17 17:04:59 +0530

    Completed Week 3 Day 2 Calculator

--- week-3/script.js
+++ week-3/script.js
@@ -1,5 +1,45 @@
-function changeText(){
 
-    document.getElementById("heading").innerHTML="JavaScript is Awesome!";
+function add(){
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerHTML = "Result: " + (num1 + num2);
 
 }
+
+function subtract(){
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerHTML = "Result: " + (num1 - num2);
+
+}
+
+function multiply(){
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerHTML = "Result: " + (num1 * num2);
+
+}
+
+function divide(){
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    if(num2 === 0){
+
+        document.getElementById("result").innerHTML = "Cannot divide by zero";
+
+    }else{
+
+        document.getElementById("result").innerHTML = "Result: " + (num1 / num2);
+
+    }
+
+}
+
